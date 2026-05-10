139641f80ed210663fab09e0f2c6a347811907de
Deekshitha-varma-gilead <deekshitha.varma@gilead.com>
2026-05-10 11:00:53 +0530

    Changed client syntax in config.py

--- src/database/config.py
+++ src/database/config.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from supabase import create_client,Client
+from supabase import create_client
 
-supabase: Client =create_client(
+supabase = create_client(
     st.secrets["SUPABASE_URL"],
     st.secrets["SUPABASE_KEY"]
 )
